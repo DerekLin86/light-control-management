c36693c3442fb2e6b6b11be299e491ec9db71b17
Derek Lin <derek.lin@careline.com.tw>
2024-12-21 10:18:04 +0800

    Adjust the style of header and left bar.

--- src/app/layout/left-bar/left-bar.component.ts
+++ src/app/layout/left-bar/left-bar.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-left-bar',
   standalone:true,
-  imports: [],
+  imports: [MatButtonModule,MatIconModule,MatDividerModule],
   templateUrl: './left-bar.component.html',
   styleUrl: './left-bar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,

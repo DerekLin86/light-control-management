3b1bce0cd9a1f5f5e1facc98ef1d78d6f650ee34
Derek Lin <derek.lin@careline.com.tw>
2025-03-02 16:43:30 +0800

    Add logout and alert button.

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -5,10 +5,12 @@ import {
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule, CommonModule],
+  imports: [MatButtonModule, CommonModule, MatIconModule, MatTooltipModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   standalone: true,
--- src/app/layout/left-bar/left-bar.component.ts
+++ src/app/layout/left-bar/left-bar.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-left-bar',

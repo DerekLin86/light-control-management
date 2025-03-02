42b7c4036ce6797da6d2bde9d1c21a76f730bd1c
Derek Lin <derek.lin@careline.com.tw>
2025-03-02 16:32:02 +0800

    Add the left tool list.

--- src/app/layout/left-bar/left-bar.component.ts
+++ src/app/layout/left-bar/left-bar.component.ts
@@ -1,14 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon'
+import { MatTooltipModule } from '@angular/material/tooltip';
 import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-left-bar',
-  standalone:true,
-  imports: [MatButtonModule,MatIconModule,MatDividerModule],
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, MatDividerModule, MatTooltipModule],
   templateUrl: './left-bar.component.html',
   styleUrl: './left-bar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LeftBarComponent { }
+export class LeftBarComponent {}

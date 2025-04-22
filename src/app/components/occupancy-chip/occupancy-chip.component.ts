15eac7f60e8a7b15b54ac1b6a41acccd5788450f
Derek Lin <derek.lin@careline.com.tw>
2025-04-22 23:15:27 +0800

    Change the occupancy icon style.

--- src/app/components/occupancy-chip/occupancy-chip.component.ts
+++ src/app/components/occupancy-chip/occupancy-chip.component.ts
@@ -1,14 +1,17 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { OccupancyStatus } from '../../types/floorPlan';
 
 @Component({
   selector: 'app-occupancy-chip',
-  imports: [],
+  imports: [MatTooltipModule],
   templateUrl: './occupancy-chip.component.html',
   styleUrl: './occupancy-chip.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OccupancyChipComponent {
   readonly status = input.required<OccupancyStatus>();
+
+  protected readonly OccupancyStatus = OccupancyStatus;
 }
